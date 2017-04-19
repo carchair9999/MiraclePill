e62df547372e17bda22c2604005c136bdc3f33eb
Carlos Delgadillo <carchair999@gmail.com>
2017-04-18 19:18:41 -0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  MiraclePillFinal
+//
+//  Created by CARLOS DELGADILLO on 10/18/16.
+//  Copyright © 2016 CARLOS Calculator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alaska","Arkansas", "Alabama","California","Maine","New York"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
+        
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+        firstText.isHidden = true
+        secondText.isHidden = true
+        buyNow.isHidden = true
+    }
+    
+
+    @IBOutlet weak var firstText: UILabel!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var buyNow: UIButton!
+
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState()) //UIControlState.normal
+        statePicker.isHidden = true
+        buyNow.isHidden = false
+        firstText.isHidden = false
+        secondText.isHidden = false
+
+    }
+    
+}
+
